033276618d4ca15e461ffb38f98ded604e8b1e27
veronicawhitty <vwhitford@college.harvard.edu>
2026-04-02 14:10:14 +0000

    Thu, Apr 2, 2026, 10:10 AM -04:00

---
+++ Protobowl.py
@@ -0,0 +1,75 @@
+### Protobowl.py
+
+# IMPORT SOME LIBRARIES
+import csv # To read our question database
+import random # For question selection
+import time # For incremental reading and "waiting" functionality
+
+# STEP 1: LOAD QUESTIONS IN FROM THE CSV DATABASE
+# Load in our questions from the csv file "questions.csv"
+def load_questions(file):
+    questions = []
+    with open(file, encoding = 'utf-8') as file:
+        # Operates like a regular reader, but maps the information in each row to
+        # a dictionary whose keys are given by the optional fieldnames parameter
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            answers = []
+            # Split answer options by semicolon (what I use in the CSV file to differentiate
+            # between answers in circumstance where they are MULTIPLE acceptable answers)
+            split_answers = row["Answer"].split(";")
+
+            for answer in split_answers:
+                answer = answer.strip()
+                if answer:
+                    answers.append(answer) # Figure out a way to standardize the answer in Step 2
+
+            prompts = []
+
+            if row["Prompts"]:
+                split_prompts = row["Prompts"].split(";")
+
+                for prompt in split_prompts:
+                    prompt = prompt.strip()
+                    if prompt:
+                        prompts.appened(answer) # Figure out a way to standardize the answer in Step 2
+
+            # Something here on the (*) for power scoring??
+            # like....
+            question_text = row["Question Text"]
+            if "(*)" in question_text: # (*) is the common symbol used in Quizbowl to indicate a "power region," so I also utilize it in my database
+                # Something here
+            else:
+                # Something similar here
+                # We should probably add whatever we do to powers to the "questions" dictionary for proper storage??
+
+    return questions
+
+
+# STEP 2: STANDARDIZE THE TEXT IN OUR ANSWWERS SO THAT USER INPUTS DON'T UPSET PYTHON
+def standardize(text):
+    text = text.lower()
+    clean_text = ""
+    for character in text:
+        if character.isalnum() or character.isspace(): # .isalnum means is alphanumeric!
+            clean_text += character
+        else:
+            clean_text += " "
+    return # I don't know what to return here just yet??
+
+# STEP 3: ASK QUESTIONS TO USER INCREMENTALLY (WORD-BY-WORD)
+
+# STEP 4: CREATE A FUNCTION TO CHECK THE USER'S ANSWER
+
+# STEP 5: FIGURE OUT MID-QUESTION BUZZING (& SCORING??)
+
+# STEP 6: FIURE OUT END-OF-QUESTION
+
+# STEP 7: ALLOW USERS TO "QUIT" WHEN THEY ARE DONE PLAYING
+
+# STEP 8: FUNCTION THAT ACTUALLY RUNS THE GAME COMPLETELY
+# def play_game():
+# Input will ultimately be the csv file to run
+if __name__ == "__main__":
+    play_game("questions.csv") # Use the imported question database CSV
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# cs32-final-project
+# CS32 Final Project: Python Terminal Protobowl
+
+## Project Overview
+This project aims to recreate the core experience of the JavaScript-operated Protobowl.com as an interactive trivia game that runs entirely in the Python terminal. In Python Terminal Protobowl, players are shown questions incementally word-by-word, can "buzz in" at any time, and receive immediate feedback on their answers. The goal is to recreate the core gameplay experience of Protobowl in a simplified form that works entirely in the Python terminal, modifying the aspects of the traditional game when needed to accommodate the irregular formatting that Python requires.
+
+## Motivation
+Quizbowl was one of my favorite extracurricular activities in high school, and one of the highlights of the "Quizbowl experience" was indubitably competing with friends with Protobowl.com, a popular online practice tool for Quizbowl competitors. I wanted to build a project that captures the fast-paced, competitive feel of Quizbowl while exploring how real-time interaction between the user and Python can be handled in a terminal environment (via real-time buzzing.) This project also allows me to work on input handling, timing, and game state management in Python, all diciplines I am intrigued by.
+
+## Intended Features
+- CSV file reading for question database
+- Incremental question display (word-by-word printing of questions)
+- Answer input and checking
+- Score tracking aross questions
+- Continuous game loop for repeated play
+- "Quit" option for when users have completed playing
+
+## Tech Stack
+- Python (CS32/CS50 IDE)
+- Terminal interface
+
+## Installation
+[ADD THIS SECTION AFTER CHATTING WITH A TF IN OFFICE HOURS]
+
+## How to Use
+[ADD MORE ONCE WE WRITE SOME MORE SUBSTANTIAL CODE]
+- The question will begin displaying gradually in the terminal
+- Press [INSERT BUZZER ACTIVATION KEY] (or following the beginning instructions message) to buzz in
+- Upon buzzing, type in your answer and click "return"
+- The program will tell you if you are correct and update your score
+- If you are incorrect, buzz as many times as needed until you achieve a correct answer OR until the question is finished being read
+
+## Contribute
+If you're reading this still: answer my abundance of questions in office hours throughout the rest of the semester (pretty please)
+
+## Credits
+Inspired by Protobowl and other quizbowl-style trivia games. An extra-special shoutout to Kevin Kwok and Ben Vest, the original programmers behind Protobowl.com, as well as Geoffrey Wu, the creator of the more modern and user-friendly QBReader interface, which has effectively replaced Protobowl in utility and thus serves as an additional reference point for this project.
