37a7c90cd93e2b5aa9f15385674d5e922063b3e3
veronicawhitty <vwhitford@college.harvard.edu>
2026-04-23 19:14:16 +0000

    Thu, Apr 23, 2026, 3:14 PM -04:00

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -5,3 +5,4 @@ from Protobowl import play_game
 # COPY AND PASTE TO RUN THIS FILE: streamlit run /workspaces/cs32-final-project/StreamlitApp.py
 
 st.title("CS32 Final Project: Python Protobowl")
+p
